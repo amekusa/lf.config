729576efb06f0e366704fccedb925fe51152622e
Satoshi Soma <noreply@amekusa.com>
2026-05-04 21:55:35 +0900

    extract: improve

--- extract.sh
+++ extract.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+_has() {
+	command -v "$1" &> /dev/null
+}
+
 _cmd() {
 	local cmd="$1"; shift
-	if command -v "$cmd" &> /dev/null; then
+	if _has "$cmd"; then
 		"$cmd" "$@"
 	else
 		echo "Command not found: '$cmd'"
@@ -23,8 +27,7 @@ _extract() {
 	if [[ "$src" =~ ^(.+)\.(zip|7z|rar|tar|tar\.t?(gz|xz|bz2?))$ ]]; then
 		local dst="${BASH_REMATCH[1]}"
 		local ext="${BASH_REMATCH[2]}"
-		echo "Extracting: '$src' ..."
-		echo "> dst: '$dst'"
+		echo "Extracting '$src' into '$dst' ..."
 		if [ -d "$dst" ]; then
 			echo "[WARN] Directory already exists."
 			local answer
@@ -50,13 +53,30 @@ _extract() {
 		mkdir -p -- "$dst" || return 1
 		echo "Created: '$dst'."
 		case "$ext" in
-		tar.bz|tar.bz2|tar.tbz|tar.tbz2) _cmd tar xjvf "$src" -C "$dst" ;;
-		tar.gz|tar.tgz) _cmd tar xzvf "$src" -C "$dst" ;;
-		tar.xz|tar.txz) _cmd tar xJvf "$src" -C "$dst" ;;
-		zip) _cmd unzip "$src" -d "$dst" ;;
-		rar) _cmd unrar x "$src" "$dst" ;;
-		7z)  _cmd 7z x "$src" -o"$dst" ;;
+			tar.bz|tar.bz2|tar.tbz|tar.tbz2) _cmd tar xjvf "$src" -C "$dst" ;;
+			tar.gz|tar.tgz) _cmd tar xzvf "$src" -C "$dst" ;;
+			tar.xz|tar.txz) _cmd tar xJvf "$src" -C "$dst" ;;
+			zip) _cmd unzip "$src" -d "$dst" ;;
+			rar) _cmd unrar x "$src" "$dst" ;;
+			7z)  _cmd 7z x "$src" -o"$dst" ;;
 		esac
+		echo
+		if [ $? -eq 0 ]; then
+			cat <<- EOF
+			[SUCCESS] Extraction done:
+			- src: '$src'
+			- dst: '$dst'
+
+			EOF
+			return 0
+		else
+			cat <<- EOF
+			[ERROR] Extraction FAILED:
+			- src: '$src'
+
+			EOF
+			return 1
+		fi
 	else
 		echo "[ERROR] Invalid file: '$src'."
 		return 1
